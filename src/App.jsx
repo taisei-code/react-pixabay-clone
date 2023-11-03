ca9e91263b3a3f06fb8c6b28974fef00c06ce1c6
Taisei <taisei96.web@gmail.com>
2023-11-03 16:25:50 +0900

    データフェッチング

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react';
 import './App.css'
+import ImageGrallery from './ImageGrallery'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+
+    // APIURL
+    const endpointURL =
+      `https://pixabay.com/api/?key=40449923-fad70dd8483601e4c537d66ad&q=${ref.current.value}&image_type=photo`;
+    // APIを叩く（データフェッチング）
+    fetch(endpointURL).then((res) => {
+      return res.json();
+    })
+      .then((data) => {
+        console.log(data.hits)
+      setFetchData(data.hits)
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <h2>My Pixabay</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" placeholder='画像を探す' ref={ref} />
+        </form>
+        <ImageGrallery fetchData={fetchData} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
